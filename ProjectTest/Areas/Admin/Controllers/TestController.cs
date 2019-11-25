060b51a76f1b5ca8c4eafa56d51c0f06e81f0001
AnimationCube <nguyentutuyen45@gmail.com>
2019-11-25 22:01:40 +0700

    Add project files.

---
+++ ProjectTest/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTest.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
