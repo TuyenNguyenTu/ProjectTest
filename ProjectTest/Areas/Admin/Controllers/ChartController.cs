5b4f5bad4c75a347e0fe5773cdebe66acd45b505
AnimationCube <nguyentutuyen45@gmail.com>
2020-01-05 20:48:22 +0700

    Commit All

---
+++ ProjectTest/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectTest.Areas.Admin.Models;
+using ProjectTest.DAO;
+using ProjectTest.Models;
+
+namespace ProjectTest.Areas.Admin.Controllers
+{
+    [Route("api/chart")]
+    public class ChartController : Controller
+    {
+        public readonly MyBlogDbContext context;
+        public ChartController(MyBlogDbContext _context)
+        {
+            context = _context;
+        }
+        [HttpGet("viewchart")]
+        [Produces("application/json")]
+        public async Task<IActionResult> viewChart()
+        {
+            try
+            {
+                var model = from p in context.Posts
+                            join cp in context.CategoryPosts on p.CategoryId equals cp.Id
+                            group p by cp.CategoryName into gp
+                            select new ChartViewModel()
+                            {
+                                CategoryName = gp.Key,
+                                ViewCount = gp.Sum(x => x.ViewCount)
+                                
+                            };
+                return  Ok(model.ToList());
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ ProjectTest/Areas/Admin/Models/ChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTest.Areas.Admin.Models
+{
+    public class ChartViewModel
+    {
+        public string CategoryName { set; get; }
+        public long ViewCount { set; get; }
+    }
+}
